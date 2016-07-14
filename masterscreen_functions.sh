3526fcd4e05e29b26942c66ffde2ce537e886d6a
cmotc <eyedeekay@i2pmail.org>
2016-07-14 07:35:02 -0400

    basic decrypt function

---
+++ masterscreen_functions.sh
@@ -0,0 +1,13 @@
+﻿#! /bin/sh
+basic_gpg_decrypt(){
+    [ ! -z "$1" ] && echo "$1" | gpg --passphrase-fd 0 $HOME/.masterscreen.gpg
+}
+generate_gpg_pwfile(){
+
+}
+generate_gpg_datakeys(){
+
+}
+unload_gpg_datask(){
+
+}
