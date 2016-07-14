10f1b48cf082ef6eb3d93e68375af19226729b45
cmotc <eyedeekay@i2pmail.org>
2016-07-14 15:28:50 -0400

    basic decrypt function

--- masterscreen_functions.sh
+++ masterscreen_functions.sh
@@ -1,13 +1,18 @@
 ﻿#! /bin/sh
 basic_gpg_decrypt(){
-    [ ! -z "$1" ] && echo "$1" | gpg --passphrase-fd 0 $HOME/.masterscreen.gpg
+    [ ! -z "$1" ] && echo "$1" | gpg --passphrase-fd 0 -d $HOME/.masterscreen.gpg
 }
 generate_gpg_pwfile(){
-
+    PASS=$(whiptail --passwordbox "please enter your secret password" 8 78 --title "password dialog" 3>&1 1>&2 2>&3)
+    PASSC=$(whiptail --passwordbox "please confirm your secret password" 8 78 --title "password dialog" 3>&1 1>&2 2>&3)
+    [ "$PASS" = "$PASSC" ] && echo $PASS | gpg --cipher-algo AES256 --password "$PASS" --output "$HOME/.masterscreen.gpg" --symmetric -
 }
 generate_gpg_datakeys(){
+    gpg --gen-key
+}
+load_gpg_datask(){
 
 }
 unload_gpg_datask(){
-
+    
 }
